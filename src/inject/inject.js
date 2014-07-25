b4efcafbb71d375b70aa33ea62ca3a7bbd2aa92f
ikanetcomet <ika.balzam@net-comet.com>
2014-07-26 02:18:03 +0800

    Icons + rename of handler + updated html + remove Naor's bull.

--- src/inject/inject.js
+++ src/inject/inject.js
@@ -1,8 +1,4 @@
 var docElement = (document.head||document.documentElement);
 var s2 = document.createElement("script");
-s2.src = chrome.extension.getURL("js/dostuff.js");
+s2.src = chrome.extension.getURL("js/canvasHandler.js");
 docElement.insertBefore(s2, docElement.firstChild);
-
-var s1 = document.createElement("script");
-s1.src = chrome.extension.getURL("js/pix-test.js");
-docElement.insertBefore(s1, docElement.firstChild);
